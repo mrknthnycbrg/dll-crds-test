e8a2338037baf0005558ce61413857f3735df82c
mrknthnycbrg <macabriga@outlook.com>
2024-02-09 07:21:04 +0800

    Edited Livewire components

--- app/Livewire/Welcome/WelcomePage.php
+++ app/Livewire/Welcome/WelcomePage.php
@@ -3,7 +3,6 @@
 namespace App\Livewire\Welcome;
 
 use App\Models\Post;
-use Illuminate\Support\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,37 +10,13 @@ class WelcomePage extends Component
 {
     use WithPagination;
 
-    public $selectedYear;
-
     public function render()
     {
-        $latestPublished = Post::where('published', true)->max('date_published');
-
-        $earliestPublished = Post::where('published', true)->min('date_published');
-
-        if ($latestPublished && $earliestPublished) {
-            $latestYear = Carbon::parse($latestPublished)->year;
-            $earliestYear = Carbon::parse($earliestPublished)->year;
-
-            $yearRange = range($latestYear, $earliestYear);
-            $years = array_combine($yearRange, $yearRange);
-        } else {
-            $years = null;
-        }
-
         $posts = Post::where('published', true)
-            ->when($this->selectedYear, function ($query) {
-                $query->whereYear('date_published', $this->selectedYear);
-            })
             ->latest('date_published')
             ->paginate(6);
 
-        return view('livewire.welcome.welcome-page', compact('posts', 'years'))
+        return view('livewire.welcome.welcome-page', compact('posts'))
             ->layout('layouts.guest');
-    }
-
-    public function updatedSelectedYear()
-    {
-        $this->resetPage();
     }
 }
