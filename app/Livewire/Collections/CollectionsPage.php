610f895dba1e07d708aa6f82d3ca88b54b796ccf
mrknthnycbrg <macabriga@outlook.com>
2024-02-11 15:09:12 +0800

    Edited Livewire components

---
+++ app/Livewire/Collections/CollectionsPage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire\Collections;
+
+use App\Models\Department;
+use Livewire\Component;
+
+class CollectionsPage extends Component
+{
+    public function render()
+    {
+        $departments = Department::with(['researches' => function ($query) {
+            $query->where('published', true)->latest('date_submitted');
+        }])->get();
+
+        return view('livewire.collections.collections-page', compact('departments'))
+            ->layout('layouts.app')
+            ->title('Collections - DLL-CRDS');
+    }
+}
