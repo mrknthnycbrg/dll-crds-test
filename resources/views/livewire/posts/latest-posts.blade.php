4979582bb3ae0501439f985511c0f718cc6ed0a3
mrknthnycbrg <macabriga@outlook.com>
2024-02-25 21:13:39 +0800

    Edited Blade views

--- resources/views/livewire/posts/latest-posts.blade.php
+++ resources/views/livewire/posts/latest-posts.blade.php
@@ -1,5 +1,5 @@
 <div class="mx-auto max-w-full bg-gray-100 px-4 py-8 sm:px-6 lg:px-8">
-    <div class="flex items-center justify-between space-x-2 py-8">
+    <div class="flex items-center justify-between space-x-2 pb-8">
         <h1 class="text-3xl font-black text-gray-900 underline decoration-amber-400 decoration-4 underline-offset-8">
             Latest News
         </h1>
@@ -9,30 +9,63 @@
         </a>
     </div>
 
-    <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
-        @forelse ($posts as $post)
-            <x-card href="{{ route('show-post', ['slug' => $post->slug]) }}" wire:navigate wire:key="{{ $post->id }}">
-                @if ($post->image_path)
-                    <img class="mx-auto aspect-video w-full rounded-md object-cover" src="{{ $post->formattedImage() }}"
-                        alt="{{ $post->title }}">
-                @else
-                    <img class="mx-auto aspect-video w-full rounded-md object-cover"
-                        src="{{ asset('images/logo.png') }}" alt="{{ $post->title }}">
-                @endif
-                <h3 class="text-base font-bold text-gray-700 group-hover:text-cyan-800">
-                    {{ $post->title }}
-                </h3>
-                <p class="text-sm font-light text-gray-700">
-                    {{ $post->formattedContent() }}
-                </p>
-                <p class="text-xs font-thin text-gray-700">
-                    {{ $post->formattedDate() }}
-                </p>
-            </x-card>
-        @empty
-            <p class="text-lg font-bold text-gray-700">
-                No posts yet.
-            </p>
-        @endforelse
+    <!-- Slider -->
+    <div class="relative" data-hs-carousel='{"loadingClasses": "opacity-0","isAutoPlay": true}'>
+        <div class="hs-carousel relative min-h-[350px] w-full overflow-hidden rounded-lg bg-white">
+            <div
+                class="hs-carousel-body absolute bottom-0 start-0 top-0 flex flex-nowrap opacity-0 transition-transform duration-700">
+                @forelse ($posts as $post)
+                    <a class="hs-carousel-slide group" href="{{ route('show-post', ['slug' => $post->slug]) }}"
+                        wire:navigate wire:key="{{ $post->id }}">
+                        <div class="flex h-full flex-col justify-end"
+                            style="background-image: url('{{ $post->image_path ? $post->formattedImage() : asset('images/featured.png') }}'); background-size: cover; background-position: center;">
+                            <div class="rounded-md bg-gray-900 bg-opacity-70 p-6 text-left text-gray-100">
+                                <h3 class="text-3xl font-bold group-hover:text-cyan-800">
+                                    {{ $post->title }}
+                                </h3>
+                                <p class="text-xl font-light">
+                                    {{ $post->formattedContent() }}
+                                </p>
+                                <p class="text-base font-thin">
+                                    {{ $post->formattedDate() }}
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                @empty
+                    <div class="hs-carousel-slide">
+                        <div class="flex h-full justify-center bg-gray-100 p-6">
+                            <span class="self-center text-4xl transition duration-700">No posts yet</span>
+                        </div>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+
+        <button
+            class="hs-carousel-prev hs-carousel:disabled:opacity-50 absolute inset-y-0 start-0 inline-flex h-full w-[46px] items-center justify-center text-gray-800 hover:bg-gray-800/[.1] disabled:pointer-events-none"
+            type="button">
+            <span class="text-2xl" aria-hidden="true">
+                <svg class="size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                </svg>
+            </span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button
+            class="hs-carousel-next hs-carousel:disabled:opacity-50 absolute inset-y-0 end-0 inline-flex h-full w-[46px] items-center justify-center text-gray-800 hover:bg-gray-800/[.1] disabled:pointer-events-none"
+            type="button">
+            <span class="sr-only">Next</span>
+            <span class="text-2xl" aria-hidden="true">
+                <svg class="size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+            </span>
+        </button>
+        <!-- End Slider -->
     </div>
 </div>
