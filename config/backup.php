0dc89284d24be06fc17de5ef3b2538b47898134b
mrknthnycbrg <macabriga@outlook.com>
2024-02-25 08:24:33 +0800

    Edited config

--- config/backup.php
+++ config/backup.php
@@ -207,7 +207,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'macabriga@outlook.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
