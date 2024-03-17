b1dca7d8567266c6d76e5d5ada60aabfc1fb0365
mrknthnycbrg <macabriga@outlook.com>
2024-03-17 16:29:49 +0800

    Edited lang

--- lang/vendor/filament-shield/en/filament-shield.php
+++ lang/vendor/filament-shield/en/filament-shield.php
@@ -31,7 +31,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'nav.group' => 'Settings',
+    'nav.group' => 'User Management',
     'nav.role.label' => 'Roles',
     'nav.role.icon' => 'heroicon-o-shield-check',
     'resource.label.role' => 'Role',
