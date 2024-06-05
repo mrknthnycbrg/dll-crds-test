5f12ca3a54a8870683d364cfc13d4db3b2ce6c65
mrknthnycbrg <macabriga@outlook.com>
2024-06-05 18:49:43 +0800

    Updated packages

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
