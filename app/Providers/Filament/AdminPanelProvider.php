c33cb6badd7461c6eb7ba837e2b351a979d968d3
mrknthnycbrg <macabriga@outlook.com>
2024-06-10 14:39:45 +0800

    Edited providers

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -47,6 +47,7 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::hex('#facc15'),
             ])
             ->databaseNotifications()
+            ->databaseNotificationsPolling('60s')
             ->unsavedChangesAlerts()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
