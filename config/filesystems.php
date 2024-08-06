750486cbef4bc229e0d83b6b284a158f2000b5be
mrknthnycbrg <macabriga@outlook.com>
2024-08-06 12:48:21 +0800

    Edited config

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,6 +49,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
